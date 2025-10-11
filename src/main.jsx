3ab9d4a79c631ccd06beda613e7d1d946615cd25
Sajed1230 <jokersajed11@gmail.com>
2025-10-11 14:44:33 +0200

    Update frontend changesd

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // ✅ use BrowserRouter
+import { BrowserRouter, HashRouter } from "react-router-dom"; // ✅ use BrowserRouter
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashChangeEvent>
+    <HashRouter>
       <App />
-    </HashChangeEvent>
+    </HashRouter>
   </StrictMode>
 );
